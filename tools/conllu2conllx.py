6036573ca2be1d9cc7cf7c72a536bc776c7f38d6
Jimmy Callin <jimmy.callin@gmail.com>
2015-11-24 18:11:29 +0100

    upgraded to ud12

--- tools/conllu2conllx.py
+++ tools/conllu2conllx.py
@@ -1,10 +1,11 @@
 import udtree
 from os.path import join
+import lang_utils
 
 # change this if you only want coarse grained dependency relations
-fine_grained_deprels = False
+fine_grained_deprels = True
 
-project_base = "/Users/jimmy/dev/edu/nlp-rod/udeval/resources/universaldependencies1-1/ud-treebanks-v1.1"
+project_base = "/Users/jimmy/dev/edu/nlp-rod/udeval/resources/universaldependencies1-2/universal-dependencies-1.2/"
 
 def convert(to_convert_files):
     for lang, files in to_convert_files.items():
@@ -14,10 +15,10 @@ def convert(to_convert_files):
         if lang == "UD_Czech" and len(files) > 1:
             file_name = "cs-ud-train" + file_ending
         else:
-            file_name = files[0].split(".")[0] + file_ending
+            file_name = files[0].split("/")[-1].split(".")[0] + file_ending
 
-        outfile = join(project_base, lang, file_name)
-        trees = udtree.from_files([join(project_base, lang, path) for path in files])
+        outfile = join(project_base, "UD_" + lang, file_name)
+        trees = udtree.from_files(files)
         with open (outfile, "w") as w:
             for tree in trees:
                 for word in tree.sentence_structure:
@@ -27,68 +28,10 @@ def convert(to_convert_files):
                     tree.postags = tree.cpostags
                 w.write("\n".join(tree.to_conllx_format(fine_grained_deprels=fine_grained_deprels)) + "\n\n")
 
-train_files = {'UD_Basque': ['eu-ud-train.conllu'],
-               'UD_Croatian': ['hr-ud-train.conllu'],
-               'UD_Danish': ['da-ud-train.conllu'],
-               'UD_Finnish': ['fi-ud-train.conllu'],
-               'UD_French': ['fr-ud-train.conllu'],
-               'UD_Greek': ['el-ud-train.conllu'],
-               'UD_Hungarian': ['hu-ud-train.conllu'],
-               'UD_Irish': ['ga-ud-train.conllu'],
-               'UD_Persian': ['fa-ud-train.conllu'],
-               'UD_Swedish': ['sv-ud-train.conllu'],
-               'UD_Bulgarian': ['bg-ud-train.conllu'],
-               'UD_Czech': ['cs-ud-train-c.conllu',
-                            'cs-ud-train-m.conllu',
-                            'cs-ud-train-v.conllu',
-                            'cs-ud-train-l.conllu'],
-               'UD_English': ['en-ud-train.conllu'],
-               'UD_Finnish-FTB': ['fi_ftb-ud-train.conllu'],
-               'UD_German': ['de-ud-train.conllu'],
-               'UD_Hebrew': ['he-ud-train.conllu'],
-               'UD_Indonesian': ['id-ud-train.conllu'],
-               'UD_Italian': ['it-ud-train.conllu'],
-               'UD_Spanish': ['es-ud-train.conllu']}
 
-dev_files = {'UD_Basque': ['eu-ud-dev.conllu'],
-               'UD_Croatian': ['hr-ud-dev.conllu'],
-               'UD_Danish': ['da-ud-dev.conllu'],
-               'UD_Finnish': ['fi-ud-dev.conllu'],
-               'UD_French': ['fr-ud-dev.conllu'],
-               'UD_Greek': ['el-ud-dev.conllu'],
-               'UD_Hungarian': ['hu-ud-dev.conllu'],
-               'UD_Irish': ['ga-ud-dev.conllu'],
-               'UD_Persian': ['fa-ud-dev.conllu'],
-               'UD_Swedish': ['sv-ud-dev.conllu'],
-               'UD_Bulgarian': ['bg-ud-dev.conllu'],
-               'UD_Czech': ['cs-ud-dev.conllu'],
-               'UD_English': ['en-ud-dev.conllu'],
-               'UD_Finnish-FTB': ['fi_ftb-ud-dev.conllu'],
-               'UD_German': ['de-ud-dev.conllu'],
-               'UD_Hebrew': ['he-ud-dev.conllu'],
-               'UD_Indonesian': ['id-ud-dev.conllu'],
-               'UD_Italian': ['it-ud-dev.conllu'],
-               'UD_Spanish': ['es-ud-dev.conllu']}
-
-test_files = {'UD_Basque': ['eu-ud-test.conllu'],
-               'UD_Croatian': ['hr-ud-test.conllu'],
-               'UD_Danish': ['da-ud-test.conllu'],
-               'UD_Finnish': ['fi-ud-test.conllu'],
-               'UD_French': ['fr-ud-test.conllu'],
-               'UD_Greek': ['el-ud-test.conllu'],
-               'UD_Hungarian': ['hu-ud-test.conllu'],
-               'UD_Irish': ['ga-ud-test.conllu'],
-               'UD_Persian': ['fa-ud-test.conllu'],
-               'UD_Swedish': ['sv-ud-test.conllu'],
-               'UD_Bulgarian': ['bg-ud-test.conllu'],
-               'UD_Czech': ['cs-ud-test.conllu'],
-               'UD_English': ['en-ud-test.conllu'],
-               'UD_Finnish-FTB': ['fi_ftb-ud-test.conllu'],
-               'UD_German': ['de-ud-test.conllu'],
-               'UD_Hebrew': ['he-ud-test.conllu'],
-               'UD_Indonesian': ['id-ud-test.conllu'],
-               'UD_Italian': ['it-ud-test.conllu'],
-               'UD_Spanish': ['es-ud-test.conllu']}
+train_files = lang_utils.get_ud_paths(project_base, type_="train", format_="conllu")
+dev_files = lang_utils.get_ud_paths(project_base, type_="dev", format_="conllu")
+test_files = lang_utils.get_ud_paths(project_base, type_="test", format_="conllu")
 
 convert(train_files)
 convert(dev_files)
